1d57e3271cff9f9279eb994b7d353e6ab43490a9
Michael Koohang <michael.s.k1001@gmail.com>
2021-03-27 11:24:38 -0400

    Include only name, username, and service in friends

--- routes/friends/friends.js
+++ routes/friends/friends.js
@@ -3,22 +3,20 @@ const router = express.Router();
 const db = require('../../model/db');
 const {map} = require('lodash');
 
-
-
 // get a user's list of friends
 router.post("/friends", (req, res) => {
   let userId = req.body.userId;
   db.User.find({username: userId}).exec()
   .then(user => {
     return Promise.all(map(user[0].friends, friend => {
-      console.log(friend);
-      return db.User.findById(friend).exec();
+      return db.User.findById(friend).select('name username service -_id').exec();
     }));
   })
   .then(data => {
     res.send(data);
   })
   .catch(err => {
+    console.log(err);
     res.status(400).send("UserID does not exist!");
   });
 });
